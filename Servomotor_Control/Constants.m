30d6a7addaed18d5e80c8092ba8a64c30c9e40da
Mak-ara <134528772+Mak-ara@users.noreply.github.com>
2025-07-21 11:35:36 +0300

    Add files via upload

---
+++ Servomotor_Control/Constants.m
@@ -0,0 +1,6 @@
+Ra=2.0; %Armature resistance in ohms
+La= 0.5; % Armature inductance
+Kb = 0.05; %Back emf constant (V⋅s/rad)
+KT = 0.015; % Torque constant (N⋅m/A)  
+J = 0.001; % Moment of inertia (kg⋅m²) 
+B0 = 0.0001; % Friction coefficient (N⋅m⋅s/rad)
